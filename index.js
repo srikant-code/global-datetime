c71cc16025cd025fd85a09e495d2164f800d2503
Srikant Sahoo <srikantsahoo3601@gmail.com>
2021-08-09 03:42:03 +0530

    :rocket: Updated 24 hours time format
    Signed-off-by: Srikant Sahoo <srikantsahoo3601@gmail.com>

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ app.get("/", (req, res) => {
   const nDate = new Date().toLocaleString("en-US", {
     timeZone: "Asia/Calcutta",
   });
-  res.send(nDate.replace("/", "-").replace("/", "-").replace(", ", "/"));
+
+const convertTime12to24 = (time12h) => {
+  const [time, modifier] = time12h.split(' ');
+
+  let [hours, minutes, seconds] = time.split(':');
+
+  if (hours === '12' || hours === '24') {
+    hours = '00';
+  }
+  if(hours > 12) return `/${hours}:${minutes}:${seconds}`
+  if (modifier === 'PM') {
+    hours = parseInt(hours, 10) + 12;
+  }
+  return `/${hours}:${minutes}:${seconds}`;
+}
+
+  let ydate = nDate.replace("/", "-").replace("/", "-")
+  ydate = ydate.split(", ")[0] + convertTime12to24(ydate.split(", ")[1]);
+
+  res.send(ydate);
 });
 
 app.listen(port, () => {
